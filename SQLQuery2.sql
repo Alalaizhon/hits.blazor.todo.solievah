613b0fa1025d7d5fc235ffb232c5ca941a5694c1
Alalaizhon <katagawa.k@gmail.com>
2024-11-02 05:00:18 +0500

    Lesson 4

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿SELECT MAX(id) FROM dbo.TaskItems;
+DBCC CHECKIDENT ('dbo.TaskItems', RESEED, 19);
+SET IDENTITY_INSERT dbo.TaskItems ON;
+INSERT INTO [dbo].[TaskItems] ([Id], [Title], [Description], [createdDate], [FinishDate]) VALUES (6, N'Задача 6', N'Описание задачи 6', N'2024-11-02 04:16:11', NULL)
+SET IDENTITY_INSERT dbo.TaskItems OFF;
+DBCC CHECKIDENT ('dbo.TaskItems', RESEED, 19);
---
+++ dbo.TaskItems.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[TaskItems] ON
+INSERT INTO [dbo].[TaskItems] ([Id], [Title], [Description], [createdDate], [FinishDate]) VALUES (1, N'Задача 1 ', N'Описание задачи 1', N'2024-11-02 04:16:11', N'2024-11-02 04:16:11')
+INSERT INTO [dbo].[TaskItems] ([Id], [Title], [Description], [createdDate], [FinishDate]) VALUES (2, N'Задача 2 ', N'Описание задачи 2', N'2024-11-02 04:16:11', NULL)
+INSERT INTO [dbo].[TaskItems] ([Id], [Title], [Description], [createdDate], [FinishDate]) VALUES (3, N'Задача 3 ', N'Описание задачи 3', N'2024-11-02 04:16:11', NULL)
+INSERT INTO [dbo].[TaskItems] ([Id], [Title], [Description], [createdDate], [FinishDate]) VALUES (4, N'Задача 4 ', N'Описание задачи 4', N'2024-11-02 04:16:11', NULL)
+INSERT INTO [dbo].[TaskItems] ([Id], [Title], [Description], [createdDate], [FinishDate]) VALUES (5, N'Задача 5', N'Описание задачи 5', N'2024-11-02 04:16:11', NULL)
+SET IDENTITY_INSERT [dbo].[TaskItems] OFF
